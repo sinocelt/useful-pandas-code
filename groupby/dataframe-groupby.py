5cc2e66ebde3a6852a08e254987777689937f8c1
Justin Barry <sinocelt@users.noreply.github.com>
2025-07-23 20:32:10 -0500

    Add files via upload

--- groupby/dataframe-groupby.py
+++ groupby/dataframe-groupby.py
@@ -13,4 +13,18 @@ df = pd.read_csv('salary.csv', delimiter='\t')
 # group by gender and get mean and median of salary
 aggregated_df = df.groupby('gender')['yearly_salary'].agg([np.mean, np.median])
 
+
+# grouped by gender
+gender_group = df.groupby('gender')
+
 print(aggregated_df)
+
+# .first() show first in each group
+# .last() show last in each group
+
+
+# get first values for each column from grouped by gender
+gender_group = df.groupby('gender')
+first = gender_group.first()
+
+print(first)
